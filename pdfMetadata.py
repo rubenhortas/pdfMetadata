f8d1189581a795b4eceac840d71d4360a15230f5
rubenhortas <rubenhortas@gmail.com>
2015-10-03 01:06:27 +0200

    Removed status and version from comments (useless)

--- pdfMetadata.py
+++ pdfMetadata.py
@@ -6,8 +6,6 @@
 @contact:   rubenhortas at gmail.com
 @github:    http://github.com/rubenhortas
 @license:   CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@status:    Developing
-@version:   alpha
 @file:      pdfMetadata.py
 """
 
