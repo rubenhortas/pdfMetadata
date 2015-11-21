62f323cd8f8e7a8216082b4d85091e01fbda7c9b
rubenhortas <rubenhortas@gmail.com>
2015-11-21 19:15:26 +0100

    Added exception handling

--- domain/log_csv.py
+++ domain/log_csv.py
@@ -10,6 +10,7 @@
 @interpreter: python3
 """
 
+from crosscutting import condition_messages
 from log import Log
 
 
@@ -31,47 +32,54 @@ class LogCsv(Log):
         __write_header(self)
             Writes CSV file header.
         """
-        f_log = open(self.file_name, 'w')
-        f_log.write('#File name, Title, Author, Creator, Subject,'
-                    ' Producer, Creation date, Modification date,'
-                    ' Encrypted, Pages, Size\n')
-        f_log.close()
+
+        try:
+            f_log = open(self.file_name, 'w')
+            f_log.write('#File name, Title, Author, Creator, Subject,'
+                        ' Producer, Creation date, Modification date,'
+                        ' Encrypted, Pages, Size\n')
+            f_log.close()
+        except Exception as ex:
+            condition_messages.print_exception(ex)
 
     def write(self, metadata):
-        f_log = open(self.file_name, 'a+')
-        if metadata.name:
-            f_log.write(metadata.name)
-        f_log.write(self.field_separator)
-        if metadata.abs_path:
-            f_log.write(metadata.abs_path)
-        f_log.write(self.field_separator)
-        if metadata.title:
-            f_log.write(metadata.title)
-        f_log.write(self.field_separator)
-        if metadata.author:
-            f_log.write(metadata.author)
-        f_log.write(self.field_separator)
-        if metadata.creator:
-            f_log.write(metadata.creator)
-        f_log.write(self.field_separator)
-        if metadata.subject:
-            f_log.write(metadata.subject)
-        f_log.write(self.field_separator)
-        if metadata.producer:
-            f_log.write(metadata.producer)
-        f_log.write(self.field_separator)
-        if metadata.creation_date:
-            f_log.write(metadata.creation_date)
-        f_log.write(self.field_separator)
-        if metadata.modification_date:
-            f_log.write(metadata.modification_date)
-        f_log.write(self.field_separator)
-        f_log.write(metadata.encrypted)
-        f_log.write(self.field_separator)
-        if metadata.num_pages:
-            f_log.write(metadata.num_pages)
-        f_log.write(self.field_separator)
-        if metadata.size:
-            f_log.write(metadata.size)
-        f_log.write('\n')
-        f_log.close()
+        try:
+            f_log = open(self.file_name, 'a+')
+            if metadata.name:
+                f_log.write(metadata.name)
+            f_log.write(self.field_separator)
+            if metadata.abs_path:
+                f_log.write(metadata.abs_path)
+            f_log.write(self.field_separator)
+            if metadata.title:
+                f_log.write(metadata.title)
+            f_log.write(self.field_separator)
+            if metadata.author:
+                f_log.write(metadata.author)
+            f_log.write(self.field_separator)
+            if metadata.creator:
+                f_log.write(metadata.creator)
+            f_log.write(self.field_separator)
+            if metadata.subject:
+                f_log.write(metadata.subject)
+            f_log.write(self.field_separator)
+            if metadata.producer:
+                f_log.write(metadata.producer)
+            f_log.write(self.field_separator)
+            if metadata.creation_date:
+                f_log.write(metadata.creation_date)
+            f_log.write(self.field_separator)
+            if metadata.modification_date:
+                f_log.write(metadata.modification_date)
+            f_log.write(self.field_separator)
+            f_log.write(metadata.encrypted)
+            f_log.write(self.field_separator)
+            if metadata.num_pages:
+                f_log.write(metadata.num_pages)
+            f_log.write(self.field_separator)
+            if metadata.size:
+                f_log.write(metadata.size)
+            f_log.write('\n')
+            f_log.close()
+        except Exception as ex:
+            condition_messages.print_exception(ex)
