f1bbdc62e0b8ff46d01df07716059964a73e82f5
rubenhortas <rubenhortas@gmail.com>
2015-11-21 19:18:05 +0100

    Added exception handling

--- domain/log_txt.py
+++ domain/log_txt.py
@@ -25,30 +25,34 @@ class LogTxt(Log):
             self._Log__rename()
 
     def write(self, metadata):
-        f_log = open(self.file_name, 'a+')
-        if metadata.name:
-            f_log.write('File: {0}'.format(metadata.name))
-        if metadata.abs_path:
-            f_log.write('Path: {0}'.format(metadata.abs_path))
-        if metadata.title:
-            f_log.write('Title: {0}'.format(metadata.title))
-        if metadata.author:
-            f_log.write('Author: {0}'.format(metadata.author))
-        if metadata.creator:
-            f_log.write('Creator: {0}'.format(metadata.creator))
-        if metadata.subject:
-            f_log.write('Subject: {0}'.format(metadata.subject))
-        if metadata.producer:
-            f_log.write('Producer: {0}'.format(metadata.producer))
-        if metadata.creation_date:
-            f_log.write('Creation date: {0}'.format(metadata.creation_date))
-        if metadata.modification_date:
-            f_log.write(
-                'Modification date: {0}'.format(metadata.modification_date))
-        f_log.write('Encrypted: {0}'.format(metadata.encrypted))
-        if metadata.num_pages:
-            f_log.write('Pages: {0}'.format(metadata.num_pages))
-        if metadata.size:
-            f_log.write('Size: {0}'.format(metadata.size))
-        f_log.write('\n')
-        f_log.close()
+        try:
+            f_log = open(self.file_name, 'a+')
+            if metadata.name:
+                f_log.write('File: {0}'.format(metadata.name))
+            if metadata.abs_path:
+                f_log.write('Path: {0}'.format(metadata.abs_path))
+            if metadata.title:
+                f_log.write('Title: {0}'.format(metadata.title))
+            if metadata.author:
+                f_log.write('Author: {0}'.format(metadata.author))
+            if metadata.creator:
+                f_log.write('Creator: {0}'.format(metadata.creator))
+            if metadata.subject:
+                f_log.write('Subject: {0}'.format(metadata.subject))
+            if metadata.producer:
+                f_log.write('Producer: {0}'.format(metadata.producer))
+            if metadata.creation_date:
+                f_log.write(
+                    'Creation date: {0}'.format(metadata.creation_date))
+            if metadata.modification_date:
+                f_log.write(
+                    'Modification date: {0}'.format(metadata.modification_date))
+            f_log.write('Encrypted: {0}'.format(metadata.encrypted))
+            if metadata.num_pages:
+                f_log.write('Pages: {0}'.format(metadata.num_pages))
+            if metadata.size:
+                f_log.write('Size: {0}'.format(metadata.size))
+            f_log.write('\n')
+            f_log.close()
+        except Exception as ex:
+            condition_messages.print_exception(ex)
