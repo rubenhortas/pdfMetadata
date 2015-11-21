fded7b4c9d0d7b12badcec7959bfaf285a647b75
rubenhortas <rubenhortas@gmail.com>
2015-11-21 19:54:01 +0100

    Changed string construction

--- crosscutting/application_messages.py
+++ crosscutting/application_messages.py
@@ -14,11 +14,11 @@ from presentation.tag import Tag
 
 
 def print_file_name(print_file_name):
-    print Tag.info + Color.bold_green + print_file_name + Color.end
+    print '{0}{1}{2}{3}'.format(Tag.info, Color.bold_green, print_file_name, Color.end)
 
 
 def print_document_info(field_name, field_value):
-    print Tag.info + field_name + ':\t\t' + field_value
+    print '{0}{1}:\t\t{2}'.format(Tag.info, field_name, field_value)
 
 
 def print_date(field_name, field_value):
@@ -26,5 +26,4 @@ def print_date(field_name, field_value):
 
 
 def print_highlighted(field_name, field_value):
-    print Tag.info + Color.bold_green + field_name + ':\t\t' + \
-        Color.bold_red + field_value + Color.end
+    print '{0}{1}{2}:\t\t{3}{4}{5}'.format(Tag.info, Color.bold_green, field_name, Color.bold_red, field_value, Color.end)
