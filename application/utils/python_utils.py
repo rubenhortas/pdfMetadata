69aca76472e6d448126bcb61ee985162890fad81
rubenhortas <rubenhortas@gmail.com>
2015-11-21 18:57:40 +0100

    Fixed coment

--- application/utils/python_utils.py
+++ application/utils/python_utils.py
@@ -6,7 +6,7 @@
 @contact: rubenhortas at gmail.com
 @github:  http://github.com/rubenhortas
 @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@file:    PythonUtils.py
+@file:    python_utils.py
 """
 
 import signal
