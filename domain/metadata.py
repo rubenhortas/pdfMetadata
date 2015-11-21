f1b6cef22a26ad015dfb7d171fb91d0a7e91d35a
rubenhortas <rubenhortas@gmail.com>
2015-11-21 19:01:31 +0100

    Fixed comentary

--- domain/metadata.py
+++ domain/metadata.py
@@ -6,7 +6,7 @@
 @contact: rubenhortas at gmail.com
 @github:  http://github.com/rubenhortas
 @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@file:    Metadata.py
+@file:    metadata.py
 """
 
 from PyPDF2 import PdfFileReader
