0da8a8f524759f2acb17b2229c2cdeac1d1ea613
rubenhortas <rubenhortas@gmail.com>
2024-07-22 20:44:09 +0200

    Refactored date_utils.py

--- application/utils/date_utils.py
+++ application/utils/date_utils.py
@@ -1,45 +1,32 @@
 import re
 from datetime import datetime
 
+_FORMAT_1 = re.compile('^D:[0-9]{14}')  # Formats: D:20071123033349Z, D:20080219165253+01'00
+_FORMAT_2 = re.compile('[0-9]{1,2}/[0-9]{1,2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}')  # Format: 3/22/2004 18:21:23
+_FORMAT_3 = re.compile('^[a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}')  # Format:  Mon Mar 14 13:55:36 2005
 
-def get_date(input_date: str) -> str:
+
+def get_date(date: str) -> str:
     """
-    get_date(input_date)
+    get_date(date)
         Converts the dates of metadata to a readable format.
 
-    :param input_date: date
-    :return:
+    :param date: input date
+    :return date_: date formatted as string
     """
-    date_format_1 = re.compile('^D:[0-9]{14}')
-    date_format_2 = re.compile('[0-9]{1,2}/[0-9]{1,2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}')
-    date_format_3 = re.compile('^[a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}')
+    date_ = date
 
-    date = input_date
-
-    """
-    Formats:
-        D:20071123033349Z
-        D:20080219165253+01'00
-    """
-    if date_format_1.match(input_date):
-        date_format1 = date_format_1.match(input_date).group(0)
+    if _FORMAT_1.match(date):
+        date_format1 = _FORMAT_1.match(date).group(0)
         d = datetime.strptime(date_format1, 'D:%Y%m%d%H%M%S')
-        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
+        date_ = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
-    """
-    Format:
-        3/22/2004 18:21:23
-    """
-    if date_format_2.match(input_date):
-        d = datetime.strptime(input_date, '%m/%d/%Y %H:%M:%S')
-        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
+    if _FORMAT_2.match(date):
+        d = datetime.strptime(date, '%m/%d/%Y %H:%M:%S')
+        date_ = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
-    """
-    Format:
-       Mon Mar 14 13:55:36 2005
-    """
-    if date_format_3.match(input_date):
-        d = datetime.strptime(input_date, '%a %b %d %H:%M:%S %Y')
-        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
+    if _FORMAT_3.match(date):
+        d = datetime.strptime(date, '%a %b %d %H:%M:%S %Y')
+        date_ = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
-    return date
+    return date_
