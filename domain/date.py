98bd1fca0e9ea00028365915d972f0b3ef68b343
rubenhortas <rubenhortas@gmail.com>
2024-07-22 20:19:34 +0200

    Fixed quotation style

--- domain/date.py
+++ domain/date.py
@@ -10,9 +10,9 @@ def format_date(input_date):
         input_date: (string) Metadata date.
      """
 
-    date_format_1 = re.compile("^D:[0-9]{14}")
-    date_format_2 = re.compile("[0-9]{1,2}/[0-9]{1,2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}")
-    date_format_3 = re.compile("^[a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}")
+    date_format_1 = re.compile('^D:[0-9]{14}')
+    date_format_2 = re.compile('[0-9]{1,2}/[0-9]{1,2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}')
+    date_format_3 = re.compile('^[a-zA-Z]{3} [a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}')
 
     date = input_date
 
@@ -23,23 +23,23 @@ def format_date(input_date):
     """
     if date_format_1.match(input_date):
         date_format1 = date_format_1.match(input_date).group(0)
-        d = datetime.strptime(date_format1, "D:%Y%m%d%H%M%S")
-        date = d.strftime("%H:%M:%S %d/%b/%Y (%a)")
+        d = datetime.strptime(date_format1, 'D:%Y%m%d%H%M%S')
+        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
     """
     Format:
         3/22/2004 18:21:23
     """
     if date_format_2.match(input_date):
-        d = datetime.strptime(input_date, "%m/%d/%Y %H:%M:%S")
-        date = d.strftime("%H:%M:%S %d/%b/%Y (%a)")
+        d = datetime.strptime(input_date, '%m/%d/%Y %H:%M:%S')
+        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
     """
     Format:
        Mon Mar 14 13:55:36 2005
     """
     if date_format_3.match(input_date):
-        d = datetime.strptime(input_date, "%a %b %d %H:%M:%S %Y")
-        date = d.strftime("%H:%M:%S %d/%b/%Y (%a)")
+        d = datetime.strptime(input_date, '%a %b %d %H:%M:%S %Y')
+        date = d.strftime('%H:%M:%S %d/%b/%Y (%a)')
 
     return date
--- presentation/utils/screen.py
+++ presentation/utils/screen.py
@@ -2,7 +2,7 @@ import os
 
 
 def clear_screen():
-    if "nt" in os.name:
-        os.system("cls")
-    elif "posix" in os.name:
-        os.system("clear")
+    if 'nt' in os.name:
+        os.system('cls')
+    elif 'posix' in os.name:
+        os.system('clear')
