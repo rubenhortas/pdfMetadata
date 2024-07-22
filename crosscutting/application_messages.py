38874d4e404e295bc8f6362defe177faa4729fae
rubenhortas <rubenhortas@gmail.com>
2024-07-22 19:55:47 +0200

    Added colorama

--- crosscutting/application_messages.py
+++ crosscutting/application_messages.py
@@ -1,19 +1,19 @@
-from presentation.color import Color
 from presentation.tag import Tag
+from colorama import Fore, Style
 
 
-def print_file_name(f):
-    print('{0}{1}{2}{3}'.format(Tag.info, Color.bold_green, f, Color.end))
+def print_file_name(name):
+    print(f"{Tag.info}{Style.BRIGHT}{Fore.GREEN}{name}{Style.RESET_ALL}")
 
 
-def print_document_info(field_name, field_value):
-    print('{0}{1}:\t\t{2}'.format(Tag.info, field_name, field_value))
+def print_document_info(name, value):
+    print(f"{Tag.info}{name}:\t\t{value}")
 
 
-def print_date(field_name, field_value):
-    print('{0}{1}:\t{2}'.format(Tag.info, field_name, field_value))
+def print_date(name, value):
+    print(f"{Tag.info}{name}:\t\t{value}")
 
 
-def print_highlighted(field_name, field_value):
-    print('{0}{1}{2}:\t\t{3}{4}{5}'.format(Tag.info, Color.bold_green, field_name, Color.bold_red, field_value,
-                                           Color.end))
+def print_highlighted(name, value):
+    print(
+        f"{Tag.info}{Style.BRIGHT}{Fore.GREEN}{name}{Style.RESET_ALL}:\t\t{Style.BRIGHT}{Fore.RED}{value}{Style.RESET_ALL}")
