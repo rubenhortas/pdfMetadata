13297e7b008352828f2c2a0be8619e623f9154ea
rubenhortas <rubenhortas@gmail.com>
2024-07-22 19:27:13 +0200

    Deleted comment

--- presentation/utils/screen.py
+++ presentation/utils/screen.py
@@ -2,11 +2,6 @@ import os
 
 
 def clear_screen():
-    """
-    clear_screen()
-        Clears the screen.
-    """
-
     if "nt" in os.name:
         os.system("cls")
     elif "posix" in os.name:
