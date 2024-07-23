e4b6dd25e8d3292480f595b72bf10a74ea045df8
rubenhortas <rubenhortas@gmail.com>
2024-07-23 21:24:20 +0200

    Added type hints

--- presentation/condition_messages.py
+++ presentation/condition_messages.py
@@ -1,13 +1,13 @@
 from presentation.tag import Tag
 
 
-def print_error(msg):
+def print_error(msg: str) -> None:
     print('{0}{1}\n'.format(Tag.error, msg))
 
 
-def print_info(msg):
+def print_info(msg: str) -> None:
     print('{0}{1}'.format(Tag.info, msg))
 
 
-def print_exception(msg):
+def print_exception(msg: str) -> None:
     print('{0}{1}\n'.format(Tag.exception, msg))
