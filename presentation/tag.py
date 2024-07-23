bf9763c58af9b6d203bb1b5c48e495126e39528b
rubenhortas <rubenhortas@gmail.com>
2024-07-23 21:24:52 +0200

    Added type hints

--- presentation/tag.py
+++ presentation/tag.py
@@ -2,6 +2,6 @@ from colorama import Fore, Style
 
 
 class Tag:
-    error = f"[{Style.BRIGHT}{Fore.RED}ERROR{Style.RESET_ALL}]"
-    exception = f"[{Style.BRIGHT}{Fore.RED}EXCEPTION{Style.RESET_ALL}]"
-    info = f"[{Style.BRIGHT}{Fore.GREEN}*{Style.RESET_ALL}]"
+    error: str = f"[{Style.BRIGHT}{Fore.RED}ERROR{Style.RESET_ALL}]"
+    exception: str = f"[{Style.BRIGHT}{Fore.RED}EXCEPTION{Style.RESET_ALL}]"
+    info: str = f"[{Style.BRIGHT}{Fore.GREEN}*{Style.RESET_ALL}]"
