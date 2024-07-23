7be077356068565f69466f3e61e105b088be4807
rubenhortas <rubenhortas@gmail.com>
2024-07-23 21:20:02 +0200

    Added type hints

--- crosscutting/utils/python_utils.py
+++ crosscutting/utils/python_utils.py
@@ -2,13 +2,13 @@ from sys import version_info
 from presentation.condition_messages import print_info
 
 
-def get_interpreter_version():
+def get_interpreter_version() -> str:
     major, minor, micro, release, serial = version_info
 
     return major
 
 
 # noinspection PyUnusedLocal
-def handle_sigint(signal, frame):
+def handle_sigint(signal, frame) -> None:
     print_info('Stopped')
     exit(0)
