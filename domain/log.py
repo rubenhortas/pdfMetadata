77eda4433fe3d4faa2101d095877cfea4751bc7c
rubenhortas <rubenhortas@gmail.com>
2024-07-23 20:46:18 +0200

    Deleted log renaming

--- domain/log.py
+++ domain/log.py
@@ -1,7 +1,3 @@
-import os
-from presentation.condition_messages import print_info
-
-
 class Log(object):
     """
     Class Log
@@ -17,9 +13,6 @@ class Log(object):
         self.field_separator = separator
         self._set_file_name(name)
 
-        if self._exists():
-            self._rename()
-
     def _set_file_name(self, name):
         if name.endswith(self.extension):
             self.name = name.replace(self.extension, '').strip()
@@ -27,30 +20,3 @@ class Log(object):
         else:
             self.name = name.strip()
             self.file_name = self.name + self.extension
-
-    def _exists(self):
-        exists = False
-
-        # Check if target data file is a directory
-        # It happens in the real world...
-        if os.path.isdir(self.file_name):
-            print_info(f"{self.file_name} is a directory")
-
-            # Create another data file
-            self.file_name = 'pdfMetadataLog'
-
-        if os.path.exists(self.file_name):
-            print_info(f"{self.file_name} already exists")
-            exists = True
-
-        return exists
-
-    def _rename(self):
-        file_name = self.name + self.extension
-        i = 1
-
-        while os.path.exists(file_name):
-            file_name = f"{self.name} ({i}){self.extension}"
-            i = i + 1
-
-        self.file_name = file_name
