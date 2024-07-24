35a3e10e049db5d2ebde9bdb907bab77121d0cac
rubenhortas <rubenhortas@gmail.com>
2024-07-24 22:51:16 +0200

    Deleted deprecations

--- domain/metadata.py
+++ domain/metadata.py
@@ -1,9 +1,8 @@
 import os
 
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 
 from domain.utils.date_utils import get_date
-from presentation import application_messages
 
 
 class Metadata:
@@ -11,7 +10,7 @@ class Metadata:
     Class Metadata
         Data and operations relatives to the pdf file.
     """
-    file_absolute_path: str = None
+    file_abs_path: str = None
     file_name: str = None
     title: str = None
     author: str = None
@@ -25,71 +24,25 @@ class Metadata:
     size: str = None
     keywords: str = None
 
-    def __init__(self, file_abs_path):
-        self.file_absolute_path = file_abs_path
-        self.file_name = os.path.basename(file_abs_path)
+    def __init__(self, file):
+        self.file_abs_path: str = file
+        self.file_name: str = os.path.basename(file)
 
-        application_messages.print_file_name(self.file_name)
-        application_messages.print_field('Path', self.file_absolute_path)
+        fd = open(self.file_abs_path, "rb")
+        document = PdfReader(fd, strict=False)
 
-        with open(file_abs_path, 'r') as file:
-            document = PdfFileReader(file, strict=False)
+        self.encrypted = 'Yes' if document.is_encrypted else 'No'
+        self.num_pages = str(len(document.pages))
+        self.size = str(os.path.getsize(file))
+        self.keywords = document.xmp_metadata.pdf_keywords
 
-            self.encrypted = 'Yes' if document.getIsEncrypted() else 'No'
-            self.num_pages = str(document.getNumPages())
-            self.size = str(os.path.getsize(file_abs_path))
-            self.keywords = document.getXmpMetadata().pdf_keywords
+        document_info = document.metadata
 
-            document_info = document.getDocumentInfo()
-
-            if document_info:
-                self.title = document_info.get('/Title', '')
-                self.author = document_info.get('/Author', '')
-                self.creator = document_info.get('/Creator', '')
-                self.producer = document_info.get('/Producer', '')
-                self.creation_date = get_date(document_info.get('/CreationDate', ''))
-                self.modification_date = get_date(document_info.get('/ModDate', ''))
-                self.subject = document_info.subject
-
-    def print_info(self) -> None:
-        """
-        print_info(self)
-            Displays the metadata in a nice format.
-        """
-        if self.title:
-            application_messages.print_field('Title', self.title)
-
-        if self.author:
-            application_messages.print_field_highlighted('Author', self.author)
-
-        if self.creator:
-            application_messages.print_field('Creator', self.creator)
-
-        if self.subject:
-            application_messages.print_field('Subject', self.subject)
-
-        if self.producer:
-            application_messages.print_field('Producer', self.producer)
-
-        if self.creation_date:
-            application_messages.print_field('Creation date', self.creation_date)
-
-        if self.modification_date:
-            application_messages.print_field('Modification date', self.modification_date)
-
-        if self.encrypted:
-            if self.encrypted == 'Yes':
-                application_messages.print_field_highlighted('Encrypted', self.encrypted)
-            else:
-                application_messages.print_field('Encrypted', self.encrypted)
-
-        if self.num_pages:
-            application_messages.print_field('Pages', self.num_pages)
-
-        if self.size:
-            application_messages.print_field('Size', '{0} bytes'.format(self.size))
-
-        if self.keywords:
-            application_messages.print_field('Keywords', self.keywords)
-
-        print()
+        if document_info:
+            self.title = document_info.get('/Title', '')
+            self.author = document_info.get('/Author', '')
+            self.creator = document_info.get('/Creator', '')
+            self.producer = document_info.get('/Producer', '')
+            self.creation_date = get_date(document_info.get('/CreationDate', ''))
+            self.modification_date = get_date(document_info.get('/ModDate', ''))
+            self.subject = document_info.subject
