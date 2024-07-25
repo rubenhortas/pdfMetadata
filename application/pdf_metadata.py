58265d9e2330ac9120374f739ffc4fb97d931140
rubenhortas <rubenhortas@gmail.com>
2024-07-25 14:00:18 +0200

    Deleted comments

--- application/pdf_metadata.py
+++ application/pdf_metadata.py
@@ -1,36 +1,3 @@
-# def scan(path: str, analyzed_files: int, total_files: int, log_txt: LogTxt | None = None,
-#          log_csv: LogCsv | None = None):
-#     for root, dirs, files in os.walk(path):
-#         for file in files:
-#             total_files += 1
-#             file_path = os.path.join(root, file)
-#             metadata = get_metadata(file_path)
-#
-#             if metadata:
-#                 analyzed_files += 1
-#
-#                 if log_txt:
-#                     log_txt.write(metadata)
-#
-#                 if log_csv:
-#                     log_csv.write(metadata)
-#
-#     return analyzed_files, total_files
-#
-#
-# def get_metadata(file_path: str) -> Metadata:
-#     metadata = None
-#
-#     if file_path.lower().endswith('.pdf'):
-#         try:
-#             metadata = Metadata(file_path)
-#             metadata.print_info()
-#         except Exception as e:
-#             print_exception(str(e))
-#     else:
-#         print_error('{0} is not a PDF file.'.format(file_path))
-#
-#     return metadata
 import os
 
 from domain.metadata import Metadata
