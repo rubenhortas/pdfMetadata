c19b0d19ae657432ff52028be353c93c71652de1
rubenhortas <rubenhortas@gmail.com>
2026-07-12 01:37:15 +0200

    style: ruff

--- presentation/cli.py
+++ presentation/cli.py
@@ -1,33 +1,37 @@
-from colorama import Style, Fore
+from typing import TYPE_CHECKING
+
+from colorama import Fore, Style
 
 from application import pdfMetadata_service
-from domain.metadata import Metadata
 from presentation.utils.file_utils import write_file
+
+if TYPE_CHECKING:
+    from domain.metadata import Metadata
 
 
 def print_metadata(metadata: Metadata) -> None:
     """
-     print_metadata(metadata: Metadata)
-        Prints the metadata in a nice format.
+    print_metadata(metadata: Metadata)
+       Prints the metadata in a nice format.
     """
     _print_file_name(metadata.file_name)
-    _print_field('Path', metadata.file_abs_path)
-    _print_field('Title', metadata.title)
-    _print_field_highlighted('Author', metadata.author)
-    _print_field('Creator', metadata.creator)
-    _print_field('Subject', metadata.subject)
-    _print_field('Producer', metadata.producer)
-    _print_field('Creation date', metadata.creation_date)
-    _print_field('Modification date', metadata.modification_date)
+    _print_field("Path", metadata.file_abs_path)
+    _print_field("Title", metadata.title)
+    _print_field_highlighted("Author", metadata.author)
+    _print_field("Creator", metadata.creator)
+    _print_field("Subject", metadata.subject)
+    _print_field("Producer", metadata.producer)
+    _print_field("Creation date", metadata.creation_date)
+    _print_field("Modification date", metadata.modification_date)
 
-    if metadata.encrypted == 'Yes':
-        _print_field_highlighted('Encrypted', metadata.encrypted)
+    if metadata.encrypted == "Yes":
+        _print_field_highlighted("Encrypted", metadata.encrypted)
     else:
-        _print_field('Encrypted', metadata.encrypted)
+        _print_field("Encrypted", metadata.encrypted)
 
-    _print_field('Pages', metadata.num_pages)
-    _print_field('Size', '{0} bytes'.format(metadata.size))
-    _print_field('Keywords', metadata.keywords)
+    _print_field("Pages", metadata.num_pages)
+    _print_field("Size", f"{metadata.size} bytes")
+    _print_field("Keywords", metadata.keywords)
 
     print()
 
@@ -46,7 +50,7 @@ def _print_file_name(name: str) -> None:
     print(f"{Style.BRIGHT}{Fore.GREEN}{name}{Style.RESET_ALL}")
 
 
-def _print_field(name: str, value: str) -> None:
+def _print_field(name: str, value: str | None) -> None:
     if value:
         print(f"{name}: {value}")
 
