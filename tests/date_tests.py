095e378cd79134c0da8c0f78cfd4b8765aa37a15
rubenhortas <rubenhortas@gmail.com>
2026-07-12 01:30:26 +0200

    style: ruff

--- tests/date_tests.py
+++ tests/date_tests.py
@@ -4,15 +4,15 @@ from domain.utils.date_utils import get_date
 
 
 class DateTest(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.dates = [
-            ('D:20071123033349Z', '03:33:49 23/Nov/2007 (Fri)'),
-            ('D:20080219165253+01\'00', '16:52:53 19/Feb/2008 (Tue)'),
-            ('3/22/2004 18:21:23', '18:21:23 22/Mar/2004 (Mon)'),
-            ('Mon Mar 14 13:55:36 2005', '13:55:36 14/Mar/2005 (Mon)'),
-            ('13:55:36 14/Mar/2005 (Mon)', '13:55:36 14/Mar/2005 (Mon)'),
+            ("D:20071123033349Z", "03:33:49 23/Nov/2007 (Fri)"),
+            ("D:20080219165253+01'00", "16:52:53 19/Feb/2008 (Tue)"),
+            ("3/22/2004 18:21:23", "18:21:23 22/Mar/2004 (Mon)"),
+            ("Mon Mar 14 13:55:36 2005", "13:55:36 14/Mar/2005 (Mon)"),
+            ("13:55:36 14/Mar/2005 (Mon)", "13:55:36 14/Mar/2005 (Mon)"),
         ]
 
-    def test_date_format(self):
+    def test_date_format(self) -> None:
         for date, expected_result in self.dates:
             self.assertEqual(expected_result, get_date(date))
